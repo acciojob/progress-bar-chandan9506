d162462fb021f3783e18a4c6da6a6df72bc856b5
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-20 20:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-//your JS code here. If required.
+let circles =document.querySelectorAll('.circle');
+let lines = document.querySelectorAll('.line');
+const nextButton=document.querySelector('#next');
+const prevButton=document.querySelector('#prev');
+
+
+let currentActive=1;
+
+
+nextButton.addEventListener("click",()=>{
+	currentActive++;
+	if(currentActive > circles.length){
+		currentActive = circles.length;
+	}
+	ui_Update();
+})
+
+
+prevButton.addEventListener("click",()=>{
+	currentActive--;
+	if(currentActive < 1){
+		currentActive = 1;
+	}
+	ui_Update();
+})
+
+
+function ui_Update(){
+	circles.forEach((circle,index)=>{
+		if (index < currentActive){
+				circle.classList.add("active");
+		}
+		else{
+			circle.classList.remove("active");
+		}
+	})
+
+	lines.forEach((line,index)=>{
+		if (index < currentActive -1){
+				line.classList.add("active");
+		}
+		else{
+			line.classList.remove("active");
+		}
+	})
+
+	if(currentActive===1){
+		prevButton.disabled = true;
+	}
+	if(currentActive===circles.length){
+		nextButton.disabled = true;
+	}
+	else{
+		prevButton.disabled = false;
+		nextButton.disabled = false;
+	}
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
